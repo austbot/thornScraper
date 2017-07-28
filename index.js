37bc4f5ff0ba171a2f5667a6956f04caeba14983
austbot <me@austbot.com>
2017-07-28 13:47:42 -0700

    init

---
+++ index.js
@@ -0,0 +1,28 @@
+//SEE ./lib for the goodies
+const {getPage$, filterOutSamePage, getUrls, findProjects} = require("./lib");
+const _ = require('highland');
+const parser = require('fast-html-parser');
+const urlToBeScraped = "https://www.wearethorn.org/our-work-to-stop-child-sexual-exploitation";
+const pageFilter = filterOutSamePage(urlToBeScraped);
+// Grab the page as a stream
+const pageStream = getPage$(urlToBeScraped)
+//Get a dom
+    .map(raw => parser.parse(raw.toString()));
+
+pageStream.fork() //Fork so the processing of both tasks is done at the same time.
+//Select A Tags and get hrefs and return as stream
+// Flatmap new stream so that items in that stream ie the url array will emit one by one
+    .flatMap(document => _(getUrls(document)))
+    //now that urls are emitting one by one, filter out same page
+    .filter(pageFilter)
+    .each((p) => {
+        console.log(p)
+    });
+
+pageStream.fork()
+    .flatMap(document => {
+        return _(findProjects(document))
+    })
+    .each((p) => {
+        console.log(p.title, " " , p.desc, "\n")
+    });
---
+++ lib.js
@@ -0,0 +1,36 @@
+//$ means it returns a stream
+const _ = require('highland');
+const request = require('request');
+const R = require('ramda');
+
+exports.getPage$ = (url) => {
+    return _(request.get(url)).collect();
+};
+
+exports.getAs = (document) => document.querySelectorAll('a');
+
+exports.mapToHref = (tags) => tags.map((tag) => tag.attributes.href);
+
+exports.getUrls = R.compose(exports.mapToHref, exports.getAs);
+
+//Curried so that I can set the function up and reuse in filter with pointFree style
+exports.filterOutSamePage = R.curry((compare, currentUrl) => {
+    if (!currentUrl) return false;
+    const cleanURL = currentUrl.replace(/"',/, '');
+    if (cleanURL[0] === '#') return false;
+    return cleanURL.indexOf(compare) === -1
+});
+
+exports.projectFilter = (element) => element.querySelector("h3 a");
+
+exports.toProject = (element) => {
+    const title = element.querySelector("h3 a").rawText;
+    const desc = element.querySelector("p").rawText;
+    return {title, desc}
+};
+
+exports.findProjects = (document) => {
+    return document.querySelectorAll(".wpb_wrapper")
+        .filter(exports.projectFilter)
+        .map(exports.toProject)
+};
